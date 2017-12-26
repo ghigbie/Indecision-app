e9d94bad428dc98dbaadcac49409b170866f5db2
George Higbie <gehigbie@gmail.com>
2017-12-26 19:30:22 +0000

    Remove const definitions to allow for complilation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const React    = require('react');
-const ReactDOM = require('react-dom');
+// const React    = require('react');
+// const ReactDOM = require('react-dom');
 
 let template = <h1>This is JSX</h1>;
 let appRoot = document.getElementById('react-container');
