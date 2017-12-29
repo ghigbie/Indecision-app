1dc5ae425db0f14ec57df9077bd07c026365f4c8
George Higbie <gehigbie@gmail.com>
2017-12-29 21:36:26 +0000

    Add new component class

---
+++ src/app.js
@@ -0,0 +1,9 @@
+class Header extends React.Component {   //extending this class give all of the features of React
+    render(){  //The component class requires render to be implimented andmust return JSX
+        return (
+            <div>
+                <h1>This is from header</h1>
+            </div>
+        );
+    }    
+}
