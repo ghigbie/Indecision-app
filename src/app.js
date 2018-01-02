630b2718fdbc0bbad220b19bca109c4ac10738e5
George Higbie <gehigbie@gmail.com>
2018-01-02 21:22:16 +0000

    Add title property toheaer component

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ class IndecisionApp extends React.Component{
     render(){
         return(
             <div>
-                <Header />
+                <Header title="Indecison App"/>
                 <Action />
                 <Options />
                 <AddOption />
@@ -11,14 +11,13 @@ class IndecisionApp extends React.Component{
     }
 }
 
-const appTitle = 'Indecison App';
 const appSubtitle = "Put your life in the hands of a computer";
 
 class Header extends React.Component {   //extending this class give all of the features of React
     render(){  //The component class requires render to be implimented andmust return JSX
         return (
             <div>
-                <h1>{appTitle}</h1>
+                <h1>{this.props.title}</h1>
                 <h2>{appSubtitle}</h2>
             </div>
         );
