0ef841040ccc9848a13739a8737b8ecc6de8b1f6
George Higbie <gehigbie@gmail.com>
2018-01-04 22:12:46 +0000

    Modify form event method

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -185,7 +185,7 @@ var AddOption = function (_React$Component6) {
         key: 'handleAddOption',
         value: function handleAddOption(e) {
             e.preventDefault();
-            var option = e.target.elements.option.value;
+            var option = e.target.elements.option.value.trim();
 
             if (option) {
                 alert(option);
